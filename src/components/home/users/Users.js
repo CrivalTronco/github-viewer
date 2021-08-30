f70c6741a1456a82d587c3fa9ef0a312d6ed6db0
CrivalTronco <crivaltronco@outlook.com>
2021-08-30 17:07:14 +0800

    Created loading component. Added animated spinner gif. Display loading component when fetching search result.

--- src/components/home/users/Users.js
+++ src/components/home/users/Users.js
@@ -1,20 +1,26 @@
 import React, { useContext } from 'react'
 import UserItem from './UserItem';
+import Loading from '../../loading/Loading'
 
 import GithubContext from '../../../context/github/githubContext'
 
 const Users = () => {
 
     const githubContext = useContext(GithubContext);
-    const { users } = githubContext;
+    const { loading, users } = githubContext;
 
-    return (
-        <div className="grid gap-4 p-4 h-uch overflow-y-auto grid-cols-3 md:grid-cols-10">
-            { users.map( user => (
-                <UserItem key={ user.id } user={ user } />
-            ))}
-        </div>
-    )
+    if (loading) {
+        return <Loading />
+    }
+    else {
+        return (
+            <div className="grid gap-4 p-4 h-uch overflow-y-auto grid-cols-3 md:grid-cols-10">
+                { users.map( user => (
+                    <UserItem key={ user.id } user={ user } />
+                ))}
+            </div>
+        )
+    }
 }
 
 export default Users
